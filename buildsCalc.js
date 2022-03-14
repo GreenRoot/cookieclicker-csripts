1923137cb21991cc319292b622518d77baac7eac
Alexander Green <1.ooo.ooo@bk.ru>
2022-03-14 23:03:57 +0300

    Update buildsCalc.js

--- buildsCalc.js
+++ buildsCalc.js
@@ -21,13 +21,13 @@ document.getElementById('commentsText').remove()
 function buildsCalc() {
     let nextUpgradePrice = document.querySelector("#tooltip > div > div:nth-child(2) > span");
     let eachTemplateProduces = document.querySelector("#tooltip > div > div.data > b:nth-child(1)");
-    let nextUpgradePriceInt = nextUpgradePrice.innerHTML.replace(/\D/gm,"");
-    let eachTemplateProducesInt = eachTemplateProduces.innerHTML.replace(/\D/gm,"");
     const toolTips = document.querySelector("#tooltip > div > div:nth-child(2) > span")
     const toolTip = document.querySelector("#tooltip > div > div.description")
     let benefit = NaN
     
     if ( (toolTips !== null) && (eachTemplateProduces !== null) ) {
+	let nextUpgradePriceInt = nextUpgradePrice.innerHTML.replace(/\D/gm,"");
+	let eachTemplateProducesInt = eachTemplateProduces.innerHTML.replace(/\D/gm,"");
         benefit = (nextUpgradePriceInt / eachTemplateProducesInt).toFixed()
         if (document.getElementsByClassName('calcHas').length <= 0) {
             toolTip.insertAdjacentHTML('beforeend', ` <b class="calcHas" style='color: green; font-size: 13px'>${benefit}</b>`)
